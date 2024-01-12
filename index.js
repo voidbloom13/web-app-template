86d6c7a40548353aff432f9f3e01d02e851d2e4e
voidbloom13 <dakota.huyghe.dev@gmail.com>
2024-01-12 01:15:40 -0500

    Added gitignore, created basic server structure in index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const port = 3000;
+
+app.use(express.static(join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '/public/index.html'))
+});
+
+app.listen(port, () => {
+  console.log("Server started at  https://localhost:" + port)
+});
