8b8caec346039742a910a70928888781675e4273
Dakota <dakota.huyghe.dev@gmail.com>
2024-01-12 22:15:44 -0500

    Added body parser import

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,6 +8,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const port = 3000;
 
 app.use(express.static(join(__dirname, '/public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '/public/index.html'))
